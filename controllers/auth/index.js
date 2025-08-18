c6c432e409ba7e976098e842759b9869c38ca68d
JuanEspinoH <juanespinoholguin@gmail.com>
2025-08-18 15:51:40 -0600

    database conected /api /route 2

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,5 +1,3 @@
 import formularioSignUp from './formularioSignUp.js'
-import formularioSignDown from './formularioSignUp.js'
-import formularioSignIN from './formularioSignUp.js'
 
 export { formularioSignUp }
