901b356cefceb841f3af9dfcfbd502cf58f7b9b3
JuanEspinoH <juanespinoholguin@gmail.com>
2025-08-18 16:05:39 -0600

    test render 1

--- api/index.js
+++ api/index.js
@@ -1,19 +1,27 @@
 import dotenv from 'dotenv'
+import path from 'path'
 dotenv.config()
 import express from 'express'
 import csrf from 'csurf'
 import cookieParser from 'cookie-parser'
 import authRoutes from '../routes/auth/index.js'
 
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+// Esto me debe de especificar mi ruta para views por que tengo problemas con vercel
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const base = dirname(__dirname)
+
 const app = express()
 app.set('x-powered-by', false)
 app.set('trust proxy', true)
 app.set('view engine', 'pug')
-app.set('views', 'views')
+app.set('views', path.join(base, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static(path.join(base, 'public')))
 app.use(cookieParser())
 app.use(csrf({ cookie: { httpOnly: true } }))
 
