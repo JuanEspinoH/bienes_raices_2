1f5cbd095ae3fca41ee631e7a662bba42a0820d5
JuanEspinoH <juanespinoholguin@gmail.com>
2025-10-01 00:51:35 -0600

    estilo de video agregados

--- constants.js
+++ constants.js
@@ -1,1 +1,5 @@
-export const videos = ['/img/videos/video1.mp4', '/img/videos/video2.mp4']
+export const videos = [
+  'https://res.cloudinary.com/donxlmpgp/video/upload/v1759278938/1_p1eifm.mp4',
+  'https://res.cloudinary.com/donxlmpgp/video/upload/v1759278929/3_qzcvzp.mp4',
+  'https://res.cloudinary.com/donxlmpgp/video/upload/v1759278929/4_ybv8ea.mp4',
+]
