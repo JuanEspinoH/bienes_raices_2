29c427604ed756b9ff2a345186e64cba721e4c3b
JuanEspinoH <juanespinoholguin@gmail.com>
2025-10-01 23:51:09 -0600

    animaciones de los elementos del inicio

--- src/scrollAnimation.js
+++ src/scrollAnimation.js
@@ -1,7 +1,6 @@
 import { videos } from '../constants.js'
 ;(function () {
   const videoContainer = document.querySelector('#videoContainer')
-
   let currentVideoIndex = 0
   videoContainer.addEventListener('ended', () => {
     videoContainer.classList.remove('opacity-100')
@@ -25,7 +24,32 @@ import { videos } from '../constants.js'
 
   document.addEventListener('DOMContentLoaded', (event) => {
     const containers = document.querySelectorAll('#propertiesContainer')
-    const selectContainer = document.querySelector('#categorias')
+    // const selectContainer = document.querySelector('#categorias')
+    const videoItemsCont = document.querySelector('#videoItemsCont')
+    const videoItem1 = document.querySelector('#videoItem1')
+    const videoItem2 = document.querySelector('#videoItem2')
+    const videoItem3 = document.querySelector('#videoItem3')
+    const videoItem4 = document.querySelector('#videoItem4')
+
+    gsap.fromTo(
+      videoItemsCont,
+      { opacity: 0, scaleY: 0 },
+      { duration: 1, opacity: 1, scaleY: 1 }
+    )
+    gsap.to(videoItem2, { opacity: 0 })
+    gsap.to(videoItem3, { opacity: 0 })
+    gsap.to(videoItem4, { opacity: 0 })
+
+    let tl = gsap.timeline({ repeat: -1 })
+    tl.fromTo(videoItem1, { opacity: 0 }, { duration: 4, opacity: 1 })
+    tl.fromTo(videoItem1, { opacity: 1 }, { duration: 4, opacity: 0 })
+    tl.fromTo(videoItem2, { opacity: 0 }, { duration: 4, opacity: 1 })
+    tl.fromTo(videoItem2, { opacity: 1 }, { duration: 4, opacity: 0 })
+    tl.fromTo(videoItem3, { opacity: 0 }, { duration: 4, opacity: 1 })
+    tl.fromTo(videoItem3, { opacity: 1 }, { duration: 4, opacity: 0 })
+    tl.fromTo(videoItem4, { opacity: 0 }, { duration: 4, opacity: 1 })
+    tl.fromTo(videoItem4, { opacity: 1 }, { duration: 4, opacity: 0 })
+
     gsap.registerPlugin(ScrollTrigger)
 
     containers.forEach((container) => {
